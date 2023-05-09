8875dfaaa4e53dee2ca657bcbd5fa5cd295c2fd4
Abinash Singh <abinash1@ualberta.ca>
2023-05-09 12:37:30 -0600

    Constructed serial struct and wrote open

--- serial-com/serial-com.cpp
+++ serial-com/serial-com.cpp
@@ -1,33 +1,37 @@
 #include "serial-com.h"
-extern "C"{
-int open_serial_port(const char *portname, speed_t baudrate)
+
+extern "C"
 {
-    int fd;
-    struct termios tty;
-    char devname[20];
 
-    // Loop through a list of possible port names
-    for (int i = 0; i < 10; i++) {
-        snprintf(devname, sizeof(devname), "%s%d", portname, i);
-        fd = open(devname, O_RDWR | O_NOCTTY | O_NONBLOCK);
-        if (fd >= 0) {
-            // Set the serial port parameters
-            tcgetattr(fd, &tty);
-            tty.c_cflag &= ~(PARENB | CSTOPB);
-            tty.c_cflag |= CS8;
-            tty.c_iflag &= ~INPCK;
-            tty.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
-            tty.c_oflag &= ~OPOST;
-            cfsetispeed(&tty, baudrate);
-            cfsetospeed(&tty, baudrate);
-            tcsetattr(fd, TCSANOW, &tty);
-            return fd;
+    int open_serial_port(const char *portname, speed_t baudrate)
+    {
+        int fd;
+        struct termios tty;
+        char devname[20];
+
+        // Loop through a list of possible port names
+        for (int i = 0; i < 10; i++)
+        {
+            snprintf(devname, sizeof(devname), "%s%d", portname, i);
+            fd = open(devname, O_RDWR | O_NOCTTY | O_NONBLOCK);
+            if (fd >= 0)
+            {
+                // Set the serial port parameters
+                tcgetattr(fd, &tty);
+                tty.c_cflag &= ~(PARENB | CSTOPB);
+                tty.c_cflag |= CS8;
+                tty.c_iflag &= ~INPCK;
+                tty.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
+                tty.c_oflag &= ~OPOST;
+                cfsetispeed(&tty, baudrate);
+                cfsetospeed(&tty, baudrate);
+                tcsetattr(fd, TCSANOW, &tty);
+                return fd;
+            }
         }
+
+        // If no port is found, return an error
+        fprintf(stderr, "Failed to open serial port %s\n", portname);
+        return -1;
     }
-
-    // If no port is found, return an error
-    fprintf(stderr, "Failed to open serial port %s\n", portname);
-    return -1;
-}
-
 }
--- serial-com/serial-com.h
+++ serial-com/serial-com.h
@@ -6,23 +6,17 @@
 #include <termios.h> // Contains POSIX terminal control definitions
 #include <unistd.h>  // write(), read(), close()
 
-struct serial_port {
-
-    int fh = -1; //filehandeler
-    
-
-
-
+struct serial_port
+{
+    int file_handle = -1; // handle to the port.
+    int mode = 0;
 };
 
-
-
-
 /**
- * 
+ *
  * Defined interface
  * All return error codes, other return values are in parameters as refrences.
- * 
+ *
  */
 
-int open_serial_port(const char *portname, speed_t baudrate);
+int open(struct serial_port &sp, const char *portname, speed_t baudrate);
