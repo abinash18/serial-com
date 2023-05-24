4465b76959fe36ed92c6cf5698f7409b0319f31a
Abinash Singh <abinash1@ualberta.ca>
2023-05-23 22:54:04 -0600

    Added defines

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/abinash/serial-com
-# Build directory: /home/abinash/serial-com/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
