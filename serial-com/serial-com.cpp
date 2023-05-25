6be8e842d6b5469650a3c74b0baa468d52d3a773
Abinash Singh <abinash1@ualberta.ca>
2023-05-24 23:45:29 -0600

    Created full open function and created final version of the serial port struct. Currently the lib is non blocking only. I intend to keep it this way.

--- serial-com/serial-com.cpp
+++ serial-com/serial-com.cpp
@@ -1,65 +1,174 @@
 #include "serial-com.h"
 
-  
-    int open_serial_port(const char *portname, speed_t baudrate)
- 
+int open_serial_port(const char *portname, speed_t baudrate)
+
+{
+    int fd;
+    struct termios tty;
+    char devname[20];
+
+    // Loop through a list of possible port names
+    for (int i = 0; i < 10; i++)
     {
-        int fd;
-        struct termios tty;
-        char devname[20];
-
-        // Loop through a list of possible port names
-        for (int i = 0; i < 10; i++)
+        snprintf(devname, sizeof(devname), "%s%d", portname, i);
+        fd = open(devname, O_RDWR | O_NOCTTY | O_NONBLOCK);
+        if (fd >= 0)
         {
-            snprintf(devname, sizeof(devname), "%s%d", portname, i);
-            fd = open(devname, O_RDWR | O_NOCTTY | O_NONBLOCK);
-            if (fd >= 0)
-            {
-                // Set the serial port parameters
-                tcgetattr(fd, &tty);
-                tty.c_cflag &= ~(PARENB | CSTOPB);
-                tty.c_cflag |= CS8;
-                tty.c_iflag &= ~INPCK;
-                tty.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
-                tty.c_oflag &= ~OPOST;
-                cfsetispeed(&tty, baudrate);
-                cfsetospeed(&tty, baudrate);
-                tcsetattr(fd, TCSANOW, &tty);
-                return fd;
-            }
+            // Set the serial port parameters
+            tcgetattr(fd, &tty);
+            tty.c_cflag &= ~(PARENB | CSTOPB);
+            tty.c_cflag |= CS8;
+            tty.c_iflag &= ~INPCK;
+            tty.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
+            tty.c_oflag &= ~OPOST;
+            cfsetispeed(&tty, baudrate);
+            cfsetospeed(&tty, baudrate);
+            tcsetattr(fd, TCSANOW, &tty);
+            return fd;
         }
-        // Loop through a list of possible port names
-        for (int i = 0; i < 10; i++)
+    }
+    // Loop through a list of possible port names
+    for (int i = 0; i < 10; i++)
+    {
+        snprintf(devname, sizeof(devname), "%s%d", portname, i);
+        fd = open(devname, O_RDWR | O_NOCTTY | O_NONBLOCK);
+        if (fd >= 0)
         {
-            snprintf(devname, sizeof(devname), "%s%d", portname, i);
-            fd = open(devname, O_RDWR | O_NOCTTY | O_NONBLOCK);
-            if (fd >= 0)
-            {
-                // Set the serial port parameters
-                tcgetattr(fd, &tty);
-                tty.c_cflag &= ~(PARENB | CSTOPB);
-                tty.c_cflag |= CS8;
-                tty.c_iflag &= ~INPCK;
-                tty.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
-                tty.c_oflag &= ~OPOST;
-                cfsetispeed(&tty, baudrate);
-                cfsetospeed(&tty, baudrate);
-                tcsetattr(fd, TCSANOW, &tty);
-                return fd;
-            }
+            // Set the serial port parameters
+            tcgetattr(fd, &tty);
+            tty.c_cflag &= ~(PARENB | CSTOPB);
+            tty.c_cflag |= CS8;
+            tty.c_iflag &= ~INPCK;
+            tty.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
+            tty.c_oflag &= ~OPOST;
+            cfsetispeed(&tty, baudrate);
+            cfsetospeed(&tty, baudrate);
+            tcsetattr(fd, TCSANOW, &tty);
+            return fd;
         }
+    }
 
-        // If no port is found, return an error
-        fprintf(stderr, "Failed to open_port serial port %s\n", portname);
-        return -1;
-    
+    // If no port is found, return an error
+    fprintf(stderr, "Failed to open_port serial port %s\n", portname);
+    return -1;
 }
 
-int open_port(SP, const char * path, speed_t baudrate, int blocking) {
-        sp.file_handle = open(path, O_RDWR | O_NOCTTY | (blocking) ? O_NONBLOCK : 0);
-        if (sp.file_handle >= 0) {
+/**
+ * struct termios
+ * 	tcflag_t c_iflag;	 	input mode flags
+ * 	tcflag_t c_oflag;		output mode flags
+ * 	tcflag_t c_cflag;		control mode flags
+ * 	tcflag_t c_lflag;		local mode flags
+ * 	cc_t c_line;			line discipline
+ * 	cc_t c_cc[NCCS];		control characters
+ *
+ */
 
-        }
-        PRINT("Failed to open_port serial port %s\n", path);
+int open_port(SP, const char *path, speed_t baudrate, int blocking)
+{
+    if (sp.file_handle != -1)
+    {
+        PRINT("Port struct already in use and not ready. Close port before reusing config.");
         return -1;
+    }
+    sp.file_handle = open(path, O_RDWR | O_NOCTTY | (blocking) ? O_NONBLOCK : 0);
+    if (sp.file_handle >= 0)
+    {
+        // Set the serial port parameters
+        tcgetattr(sp.file_handle, &sp.tty);
+
+        if (sp.parity)
+            sp.tty.c_cflag &= ~PARENB;
+        else
+            sp.tty.c_cflag |= PARENB;
+
+        if (sp.two_stop_bits)
+            sp.tty.c_cflag |= CSTOPB;
+        else
+            sp.tty.c_cflag &= ~CSTOPB;
+
+        switch (sp.num_bits)
+        {
+        case 5:
+            sp.tty.c_cflag |= CS5;
+            break;
+        case 6:
+            sp.tty.c_cflag |= CS6;
+            break;
+        case 7:
+            sp.tty.c_cflag |= CS7;
+            break;
+        case 8:
+            sp.tty.c_cflag |= CS8;
+            break;
+        default:
+            sp.tty.c_cflag |= CS8;
+            break;
+        }
+
+        if (sp.flow_control)
+            sp.tty.c_cflag |= CRTSCTS;
+        else
+            sp.tty.c_cflag &= ~CRTSCTS;
+
+        sp.tty.c_iflag &= ~INPCK;
+
+        // disable software flow control
+        sp.tty.c_iflag &= ~(IXON | IXOFF | IXANY);
+
+        // Disable input handling
+        sp.tty.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL);
+
+        sp.tty.c_lflag &= ~(ICANON | ECHO | ECHOE | ECHONL | ISIG);
+
+        // prevent output processing.
+        sp.tty.c_oflag &= ~OPOST;
+
+        cfsetispeed(&sp.tty, baudrate);
+        cfsetospeed(&sp.tty, baudrate);
+
+        if (tcsetattr(sp.file_handle, TCSANOW, &sp.tty) != 0)
+        {
+            PRINT("Error %i from tcsetattr: %s\n", errno, strerror(errno));
+            return -1;
+        }
+        sp.baudrate = baudrate;
+
+        return 0;
+    }
+    PRINT("Failed to open_port serial port %s\n", path);
+    return -1;
 }
+
+int close_port(SP)
+{
+    if (sp.file_handle == -1)
+    {
+        PRINT("Port already closed.");
+        return -1;
+    }
+    close(sp.file_handle);
+    sp.file_handle = -1;
+    return 0;
+}
+// https://man7.org/linux/man-pages/man2/ioctl.2.html
+int input_waiting(SP)
+{
+    int bytes_waiting;
+    // It returns -1 if error occured errno will indicate error.
+    return ioctl(sp.file_handle, FIONREAD, &bytes_waiting);
+}
+
+void set_parity(SP, BOOL parity)
+{
+}
+
+void set_stop_bits(SP, BOOL stop_bits)
+{
+}
+
+void set_num_bits(SP, BOOL bits) {}
+
+void set_flow_control(SP, BOOL flow_control) {}
+
+void set_baudrate(SP, int baudrate) {}
--- serial-com/serial-com.h
+++ serial-com/serial-com.h
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 
-#include <errno.h>   // Error integer and strerror() function
-#include <fcntl.h>   // Contains file controls like O_RDWR
+#include <errno.h> // Error integer and strerror() function
+#include <fcntl.h> // Contains file controls like O_RDWR
+#include <sys/ioctl.h>
 #include <termios.h> // Contains POSIX terminal control definitions
 #include <unistd.h>  // write(), read(), close()
-
 #define SP struct serial_port &sp
 
 #define PRINT(fmt, ...)                          \
@@ -15,13 +15,34 @@
             fprintf(stderr, fmt, ##__VA_ARGS__); \
     } while (0)
 
+/**
+ * @brief Boolean for easy types.
+ */
+enum BOOL
+{
+    TRUE = 1,
+    FALSE = 0
+};
+
 struct serial_port
 {
     int file_handle = -1; // handle to the port.
     int mode = 0;
     char *path; // Physical system path of com port
+    struct termios tty;
+    int baudrate;
+
+    BOOL parity = FALSE,
+         /** If TRUE two stop bits are used, if FALSE only one is used*/
+        two_stop_bits = FALSE;
+    /** Number bits 5 6 7 or 8 */
+    int num_bits = 8;
+
+    BOOL flow_control = FALSE;
 };
 
+// TODO: Save terminal config and reapply once port is closed
+// TODO: Time outs.
 /**
  *
  * Defined interface
@@ -29,4 +50,30 @@ struct serial_port
  *
  */
 
-int open_serial_port(const char *portname, speed_t baudrate);
+int open_serial_port(const char *portname, int baudrate);
+
+int open_port(SP, const char *portname, speed_t baudrate);
+
+/**
+ * @brief Close the provided port and free it.
+ * @param Port struct to free
+ * @return error code.
+ */
+int close_port(SP);
+
+/**
+ * @brief Checks if there is bytes waiting in input buffer.
+ * @param Port to check.
+ * @return Number of bytes waiting (>=0). or -1 if error.
+ */
+int input_waiting(SP);
+
+void set_parity(SP, BOOL parity);
+
+void set_stop_bits(SP, BOOL stop_bits);
+
+void set_num_bits(SP, BOOL bits);
+
+void set_flow_control(SP, BOOL flow_control);
+
+void set_baudrate(SP, int baudrate);
